f757e3295b3fcac6556047f34222d9c9488174c5
Hector Rondon <hrondon@teclib.com>
2018-04-24 18:17:05 -0400

    feat(notifications): create notifications context
    Signed-off-by: Hector Rondon <hrondon@teclib.com>

---
+++ src/providers/NotificationsProvider/index.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+
+const NotificationsContext = React.createContext()
+
+export const NotificationsConsumer = NotificationsContext.Consumer
+
+export class NotificationsProvider extends Component {
+    state = {
+        notification: {
+            title: '',
+            body: '',
+            type: 'info'
+        },
+        show: false,
+        setNotification: (notification) => {
+            this.setState({
+                show: true,
+                notification,
+            },
+            () => {})
+        },
+        hidenNotification: () => {
+            this.setState({
+                show: false,
+            },
+            () => { })
+        },
+    }
+
+    render() {
+        return (
+            <NotificationsContext.Provider value={{ state: this.state }}>
+                {this.props.children}
+            </NotificationsContext.Provider>
+        );
+    }
+}
