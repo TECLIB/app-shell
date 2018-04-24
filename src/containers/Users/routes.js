50137da49d2b7b1a865172fbbaec7b14ed5c379f
Hector Rondon <hrondon@teclib.com>
2018-04-23 22:45:51 -0400

    feat(users): create users container
    Signed-off-by: Hector Rondon <hrondon@teclib.com>

---
+++ src/containers/Users/routes.js
@@ -0,0 +1,20 @@
+import EmptyMessage from '../../components/EmptyMessage'
+import UsersContent from './components/UsersContent'
+import { I18n } from 'react-i18nify'
+
+const routes = [
+    {
+        path: '/',
+        name: I18n.t('commons.no_selection'),
+        component: EmptyMessage,
+        exact: true
+    },
+    {
+        path: '/:id',
+        name: I18n.t('commons.selected'),
+        component: UsersContent,
+        exact: true
+    }
+]
+
+export default routes
