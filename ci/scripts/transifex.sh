2b58efb06d3a861d8b0ac5877b3b77249ca8ec8a
Hector Rondon <hrondon@teclib.com>
2018-04-23 22:49:57 -0400

    ci(circle): add scripts to deploy
    Signed-off-by: Hector Rondon <hrondon@teclib.com>

---
+++ ci/scripts/transifex.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+pip install transifex-client
+echo $'[https://www.transifex.com]\nhostname = https://www.transifex.com\nusername = '"$TRANSIFEX_USER"$'\npassword = '"$TRANSIFEX_API_TOKEN"$'\ntoken = '"$TRANSIFEX_API_TOKEN"$'\n' > ~/.transifexrc
+tx pull -a
+tx push -s
