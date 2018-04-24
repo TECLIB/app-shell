abda1199bf8cd3a4ccd146364440e480345f29fa
Hector Rondon <hrondon@teclib.com>
2018-04-24 18:11:03 -0400

    feat(authentication): create authentication context
    Signed-off-by: Hector Rondon <hrondon@teclib.com>

---
+++ src/providers/AuthenticationProvider/index.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react'
+import publicURL from '../../shared/publicURL'
+
+const AuthenticationContext = React.createContext()
+
+export const AuthenticationConsumer = AuthenticationContext.Consumer
+
+export class AuthenticationProvider extends Component {
+    state = {
+        configurationPassword: {},
+        captcha: {},
+        sessionToken: localStorage.getItem('sessionToken') ? localStorage.getItem('sessionToken') : undefined,
+        currentUser: localStorage.getItem('currentUser') ? JSON.parse(localStorage.getItem('currentUser')) : undefined,
+        setCurrentUser: (currentUser, sessionToken) => {
+            this.setState({ currentUser, sessionToken }, () => {
+                localStorage.setItem('currentUser', JSON.stringify(currentUser))
+                localStorage.setItem('sessionToken', sessionToken)
+                this.props.children.props.history.push(`${publicURL}/app`)
+            })
+        },
+        logout: () => {
+            this.setState({
+                currentUser: undefined,
+                sessionToken: undefined
+            },
+                () => {
+                    localStorage.removeItem('currentUser')
+                    localStorage.removeItem('sessionToken')
+                    this.props.children.props.history.push(`${publicURL}/`)
+                })
+        },
+    }
+
+    render() {
+        return (
+            <AuthenticationContext.Provider value={{ state: this.state }}>
+                {this.props.children}
+            </AuthenticationContext.Provider> 
+        );
+    }
+}
