9b58b8fe5446dc266fa1afe435f8198b4ce184cf
Hector Rondon <hrondon@teclib.com>
2018-04-23 22:20:44 -0400

    refactor(favicon): use teclib logo
    Signed-off-by: Hector Rondon <hrondon@teclib.com>

--- src/index.js
+++ src/index.js
@@ -1,8 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Router } from 'react-router-dom'
+import history from './shared/history'
+import RootApp from './app/RootApp'
+import { Provider } from 'react-redux'
+import {
+    createStore,
+    applyMiddleware,
+    compose
+} from 'redux'
+import thunk from 'redux-thunk'
+import './assets/styles/css/index.css'
+import rootReducer from './store'
+import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    (
+        <Provider store={
+            createStore(rootReducer, (
+                (DevTool) => { // Enable Redux DevTool if are available
+                    return (process.env.NODE_ENV === 'development' && typeof (DevTool) === 'function')
+                        ? DevTool
+                        : compose
+                })(window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__)(
+                    applyMiddleware(thunk)
+                ))
+        }>
+            <Router history={history}>
+                <RootApp />
+            </Router>
+        </Provider>
+    ),
+    document.getElementById('root')
+)
+
+// ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
