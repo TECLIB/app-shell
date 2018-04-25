32f62e050e477c6bbb996fb83074d9e2abb294d5
Hector Rondon <hrondon@teclib.com>
2018-04-25 19:35:30 -0400

    feat(notifications): create HOC to handle notifications
    Signed-off-by: Hector Rondon <hrondon@teclib.com>

---
+++ src/hoc/withNotifications/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { NotificationsConsumer } from '../../providers/NotificationsProvider'
+
+const withNotifications = WrappedComponent => {
+    const notification = props => {
+
+        return (
+            <NotificationsConsumer>
+                {(value) => <WrappedComponent {...props} toast={value.state} />}
+            </NotificationsConsumer>
+        )
+    }
+
+    return notification
+}
+
+export default withNotifications
