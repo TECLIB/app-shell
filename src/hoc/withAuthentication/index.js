9b2cca9053a704375ed54306c348ff6411ded45b
Hector Rondon <hrondon@teclib.com>
2018-04-25 15:17:16 -0400

    feat(authentication): create HOC to authentication context
    Signed-off-by: Hector Rondon <hrondon@teclib.com>

---
+++ src/hoc/withAuthentication/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { AuthenticationConsumer } from '../../providers/AuthenticationProvider'
+
+const withAuthentication = WrappedComponent => {
+    const authentication = props => {
+
+        return (
+            <AuthenticationConsumer>
+            { (value) => <WrappedComponent {...props} authentication={value.state} /> }
+            </AuthenticationConsumer>
+        )
+    }
+
+    return authentication
+}
+
+export default withAuthentication
