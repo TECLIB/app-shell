fc3d576395b99a2e505f3f6710daafc8f595adfd
Hector Rondon <hrondon@teclib.com>
2018-04-25 14:24:32 -0400

    feat(routes): validate authentication
    Signed-off-by: Hector Rondon <hrondon@teclib.com>

--- src/components/GenerateRoutes/PropsRoute/index.js
+++ src/components/GenerateRoutes/PropsRoute/index.js
@@ -1,13 +1,20 @@
 import React from 'react'
 import renderMergedProps from '../renderMergerProps/renderMergedProps'
+import PrivateRoute from '../PrivateRoute'
 import { Route } from 'react-router-dom'
 
 const PropsRoute = ({ component, ...rest }) => {
-  return (
-    <Route {...rest} render={routeProps => {
-      return renderMergedProps(component, routeProps, rest);
-    }}/>
-  );
+  if (rest.authenticate) {
+    return (
+      <PrivateRoute {...rest} component={component} redirectTo="/" />
+    )
+  } else {
+    return (
+      <Route {...rest} render={routeProps => {
+        return renderMergedProps(component, routeProps, rest);
+      }} />
+    )
+  }
 }
 
 export default PropsRoute
