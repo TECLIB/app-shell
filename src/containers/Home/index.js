3257117eebd6d4b1e84818b508a20fcf88fbf64d
Hector Rondon <hrondon@teclib.com>
2018-04-26 10:06:06 -0400

    refactor(store): remove redux from app
    Signed-off-by: Hector Rondon <hrondon@teclib.com>

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -3,19 +3,9 @@ import PropTypes from 'prop-types'
 import { I18n } from 'react-i18nify'
 import withHandleMessages from '../../hoc/withHandleMessages'
 import Loading from '../../components/Loading'
-import { uiSetNotification } from '../../store/ui/actions'
-import { bindActionCreators } from 'redux'
-import { connect } from 'react-redux'
 import EmptyMessage from '../../components/EmptyMessage'
 import ContentPane from '../../components/ContentPane'
 import delay from '../../shared/delay'
-
-function mapDispatchToProps(dispatch) {
-  const actions = {
-      setNotification: bindActionCreators(uiSetNotification, dispatch)
-  }
-  return { actions }
-}
 
 class Dashboard extends Component {
   constructor (props) {
@@ -53,7 +43,4 @@ Dashboard.propTypes = {
   history: PropTypes.object.isRequired
 }
 
-export default connect(
-  null,
-  mapDispatchToProps
-)(withHandleMessages(Dashboard))
+export default withHandleMessages(Dashboard)
