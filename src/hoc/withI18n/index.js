d8ede37a3463c71170fca70044df8b3ac269e38b
Hector Rondon <hrondon@teclib.com>
2018-04-25 20:17:54 -0400

    feat(i18n): create HOC to handle i18n
    Signed-off-by: Hector Rondon <hrondon@teclib.com>

---
+++ src/hoc/withI18n/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { I18nConsumer } from '../../providers/I18nProvider'
+
+const withI18n = WrappedComponent => {
+    const i18n = props => {
+
+        return (
+            <I18nConsumer>
+                {(value) => <WrappedComponent {...props} language={value.state} />}
+            </I18nConsumer>
+        )
+    }
+
+    return i18n
+}
+
+export default withI18n
