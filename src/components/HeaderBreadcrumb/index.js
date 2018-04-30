c9a861f5ae68a03b5035aad81f04c5e54102c1d8
Hector Rondon <hrondon@teclib.com>
2018-04-30 10:59:49 -0400

    refactor(breadcrumb): use Breadcrumb component from fabric
    Signed-off-by: Hector Rondon <hrondon@teclib.com>

--- src/components/HeaderBreadcrumb/index.js
+++ src/components/HeaderBreadcrumb/index.js
@@ -4,60 +4,62 @@ import { Link } from 'react-router-dom'
 import { I18n } from "react-i18nify"
 import publicURL from '../../shared/publicURL'
 import { Icon } from 'office-ui-fabric-react/lib/Icon'
+import { Breadcrumb } from 'office-ui-fabric-react/lib/Breadcrumb'
 
 class HeaderBreadcrumb extends React.Component {
 
-  breadcrumbs () {
-    let breadcrumbs = []
-    const addresses = this.props.history.location.pathname.split("/")
-    for (let index = (publicURL === '') ? 2 : 3; index < addresses.length; index++) {
-      let path = `${publicURL}/app`
-      for (let i = (publicURL === '') ? 2 : 3; i < index + 1; i++) {
-        path += `/${addresses[i]}`
-      }
-      breadcrumbs.push (
-        <React.Fragment key={path}>
-          <span className="header-breadcrumb-separator">/</span>
-          <span>
-            <Link to={path}>
-              {addresses[index].replace(/\b\w/g, l => l.toUpperCase())}
-            </Link>
-          </span>
-        </React.Fragment>
-      )
+    renderItem = (eventObject) => {
+        return (<Link to={eventObject.href}>
+            {eventObject.text}
+        </Link>)
     }
 
-    return breadcrumbs
-  }
-  
-  render () {
+    breadcrumbs() {
+        let breadcrumbs = []
+        breadcrumbs.push({ text: I18n.t('commons.dashboard'), key: `${publicURL}/app`, href: `${publicURL}/app` })
+        const customDivider = () => <span className="header-breadcrumb-separator">&nbsp;/&nbsp;</span>
+        const addresses = this.props.history.location.pathname.split("/")
 
-    return (
-      <header className="header-block">
-  
-        <div className="header-icon">
-          <Icon iconName="CollapseMenu" onClick={this.props.handleToggleExpand} />
-        </div>
-  
-        <nav className="header-breadcrumb">
-          <span>
-            <Link to={`${publicURL}/app`}>
-              {I18n.t('commons.dashboard')}
-            </Link>
-          </span>
+        for (let index = (publicURL === '') ? 2 : 3; index < addresses.length; index++) {
+            let path = `${publicURL}/app`
+            for (let i = (publicURL === '') ? 2 : 3; i < index + 1; i++) {
+                path += `/${addresses[i]}`
+            }
+            breadcrumbs.push({ text: addresses[index].replace(/\b\w/g, l => l.toUpperCase()), key: path, href: path })
+        }
 
-          {this.breadcrumbs()}
+        let componentBreadcrumbs = (
+            <div style={{ display: 'inline-block' }}>
+                <Breadcrumb 
+                    className="header-breadcrumb-separator"
+                    items={breadcrumbs}
+                    onRenderItem={this.renderItem}
+                    dividerAs={customDivider}
+                    maxDisplayedItems={3}
+                />
+            </div>)
 
-        </nav>
-  
-      </header>
-    )
-  }
+        return componentBreadcrumbs
+    }
+
+    render() {
+        return (
+            <header className="header-block">
+                <div className="header-icon">
+                    <Icon iconName="CollapseMenu" onClick={this.props.handleToggleExpand} />
+                </div>
+
+                <nav className="header-breadcrumb">
+                    {this.breadcrumbs()}
+                </nav>
+            </header>
+        )
+    }
 }
 
 HeaderBreadcrumb.propTypes = {
-  handleToggleExpand: PropTypes.func.isRequired,
-  history: PropTypes.object.isRequired
+    handleToggleExpand: PropTypes.func.isRequired,
+    history: PropTypes.object.isRequired
 }
- 
+
 export default HeaderBreadcrumb
