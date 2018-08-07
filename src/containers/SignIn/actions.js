70f72a380bced0295d8d011cc42caf3cf7d7186f
Hector Rondon <hrondon@teclib.com>
2018-08-07 15:39:52 -0400

    refactor(signIn): handle user signIn
    Signed-off-by: Hector Rondon <hrondon@teclib.com>

--- src/containers/SignIn/actions.js
+++ src/containers/SignIn/actions.js
@@ -18,17 +18,28 @@ export const changePhase = (ctx, newPhase) => {
 export const handleFormSubmit = (ctx, event) => {
   event.preventDefault()
 
-  const user = {
-    id: 1,
-    name: 'User App',
-    email: 'user@teclib.com',
-    picture: null,
-  }
-
-  ctx.props.auth.setCurrentUser(user, 'token')
-  ctx.props.toast.setNotification({
-    title: 'Teclib',
-    body: 'Welcome!',
-    type: 'success',
+  ctx.setState({
+    isLoading: true,
+  }, () => {
+    ctx.props.auth.fetchSignIn(ctx.state.username, ctx.state.password)
+      .then(() => {
+        ctx.props.toast.setNotification({
+          title: 'Teclib',
+          body: 'Welcome!',
+          type: 'success',
+        })
+      })
+      .catch(() => {
+        ctx.setState({
+          isLoading: false,
+          password: '',
+        }, () => {
+          ctx.props.toast.setNotification({
+            title: 'Teclib',
+            body: 'Error!',
+            type: 'alert',
+          })
+        })
+      })
   })
 }
