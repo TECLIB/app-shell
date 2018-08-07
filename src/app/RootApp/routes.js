7f7f78ffc5c0f00d1dd932528e289ee6ef3bf0c7
Hector Rondon <hrondon@teclib.com>
2018-08-07 16:50:26 -0400

    refactor(routes): add signUp route
    Signed-off-by: Hector Rondon <hrondon@teclib.com>

--- src/app/RootApp/routes.js
+++ src/app/RootApp/routes.js
@@ -1,11 +1,18 @@
 import publicURL from 'shared/publicURL'
 import SignIn from 'containers/SignIn'
+import SignUp from 'containers/SignUp'
 import asyncAdminApp from 'async/asyncAdminApp'
 
 const routes = [
   {
     path: `${publicURL}/`,
     component: SignIn,
+    exact: true,
+    authenticate: false,
+  },
+  {
+    path: `${publicURL}/SignUp`,
+    component: SignUp,
     exact: true,
     authenticate: false,
   },
