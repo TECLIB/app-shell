b2c0043d3c24eea8b0b5f13fd3383332c33bf4bb
Hector Rondon <hrondon@teclib.com>
2018-08-15 15:57:00 -0400

    refactor(contact): handle list actions
    Signed-off-by: Hector Rondon <hrondon@teclib.com>

--- src/containers/Contact/components/ContactList.js
+++ src/containers/Contact/components/ContactList.js
@@ -1,40 +1,33 @@
 import React, { PureComponent } from 'react'
+import PropTypes from 'prop-types'
 import {
   DetailsList,
-  DetailsListLayoutMode,
   Selection,
   SelectionMode,
 } from 'office-ui-fabric-react/lib/DetailsList'
-import { lorem } from 'office-ui-fabric-react/lib/utilities/exampleData'
+import { CommandBar } from 'office-ui-fabric-react/lib/CommandBar'
+import I18n from 'shared/i18n'
+import publicURL from 'shared/publicURL'
+import delay from 'shared/delay'
+import contacts from 'data/contacts.json'
 import Loader from 'components/Loader'
+import EmptyMessage from 'components/EmptyMessage'
 import ContactItemList from './ContactItemList'
-import ToolBar from './ToolBar'
-
-const items = []
 
 /**
- * Component with the users section
- * @class ToolBarlist
+ * Component with the contacts section
+ * @class ContactList
  * @extends PureComponent
  */
-class ContactList extends PureComponent {
+export default class ContactList extends PureComponent {
   constructor(props) {
     super(props)
 
     this.selection = new Selection({
-      onSelectionChanged: () => {},
+      onSelectionChanged: () => {
+        this.handleSelectionChanged()
+      },
     })
-
-    //  Populate with items for demos.
-    if (items.length === 0) {
-      for (let i = 0; i < 30; i += 1) {
-        const userName = lorem(2).replace(/[^a-zA-Z ]/g, '')
-
-        items.push({
-          name: userName,
-        })
-      }
-    }
 
     this.columns = [
       {
@@ -48,41 +41,270 @@ class ContactList extends PureComponent {
     ]
 
     this.state = {
+      selectedItems: this.props.selectedItems,
       isLoading: false,
-      itemList: ['hola'],
-      items,
-      isModalSelection: false,
+      itemList: [],
+    }
+    this.order = 'ASC'
+    this.pagination = {
+      start: 0,
+      page: 1,
+      count: 15,
+    }
+  }
+
+  componentDidMount() {
+    this.handleRefresh()
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.props.action === 'reload') {
+      this.props.changeAction(null)
+      this.handleRefresh()
+    }
+
+    if (prevState.selectedItems.length > 0 && this.state.selectedItems.length === 0 && !this.props.selectionMode) {
+      this.selection.setAllSelected(false)
+    }
+  }
+
+  componentWillUnmount() {
+    this.props.changeSelectedItems([])
+    this.props.changeSelectionMode(false)
+  }
+
+  static getDerivedStateFromProps(nextProps, prevState) {
+    return {
+      ...prevState,
+      selectedItems: nextProps.selectedItems,
+    }
+  }
+
+  handleToggleSelectionMode = () => {
+    this.props.history.push(`${publicURL}/app/contacts`)
+    this.props.changeSelectionMode(!this.props.selectionMode)
+    this.props.changeSelectedItems([])
+    this.selection.setAllSelected(false)
+  }
+
+  handleSelectionChanged = () => {
+    const itemSelected = []
+    if (this.selection.getSelectedCount() > 0) {
+      for (const item of this.selection.getSelection()) {
+        itemSelected.push(item)
+      }
+      this.props.changeSelectedItems(itemSelected)
+      if (itemSelected.length === 1 && !this.props.selectionMode) {
+        this.props.history.push(`${publicURL}/app/contacts/${itemSelected[0]['User.id']}`)
+      }
+      if (itemSelected.length > 1 && !this.props.selectionMode) {
+        this.props.history.push(`${publicURL}/app/contacts/edit/`)
+      }
+    }
+  }
+
+  handleRefresh = async () => {
+    try {
+      this.selection.setAllSelected(false)
+      this.props.history.push(`${publicURL}/app/contacts`)
+      this.props.changeSelectedItems([])
+      this.pagination = {
+        start: 0,
+        page: 1,
+        count: 15,
+      }
+      this.setState({
+        isLoading: true,
+      })
+      await delay(2000)
+      const response = contacts
+
+      this.order = response.order
+      this.setState({
+        isLoading: false,
+        itemList: response.data,
+      })
+    } catch (e) {
+      this.order = 'ASC'
+      this.setState({
+        isLoading: false,
+      })
+    }
+  }
+
+  handleEdit = () => {
+
+  }
+
+  dialogDelete = () => {
+    this.props.confirmation.showDialog({
+      title: I18n.t('users.delete'),
+      message: `${this.state.selectedItems.length} ${I18n.t('commons.users')}`,
+      isOk: this.handleDelete,
+      isCancel: this.cancelDelete,
+    })
+  }
+
+  cancelDelete = () => {
+    this.selection.setAllSelected(false)
+    this.props.changeSelectionMode(false)
+    this.props.changeSelectedItems([])
+  }
+
+  handleDelete = async () => {
+    this.setState({
+      isLoading: true,
+    }, async () => {
+      try {
+        this.props.toast.setNotification({
+          title: I18n.t('commons.success'),
+          body: I18n.t('notifications.elements_successfully_removed'),
+          type: 'success',
+        })
+        this.props.changeSelectionMode(false)
+        this.props.changeSelectedItems([])
+        this.props.changeAction('reload')
+      } catch (error) {
+        this.props.toast.setNotification(this.props.handleMessage({ type: 'alert', message: error }))
+        this.selection.setAllSelected(false)
+        this.props.changeSelectionMode(false)
+        this.props.changeSelectedItems([])
+        this.setState(({
+          isLoading: false,
+        }))
+      }
+    })
+  }
+
+  handleSort = async () => {
+    try {
+      this.selection.setAllSelected(false)
+      this.props.history.push(`${publicURL}/app/contacts`)
+      this.props.changeSelectedItems([])
+      this.pagination = {
+        start: 0,
+        page: 1,
+        count: 15,
+      }
+      this.setState({
+        isLoading: true,
+      })
+      await delay(2000)
+      const response = contacts
+
+      this.order = response.order
+      this.setState({
+        isLoading: false,
+        itemList: response.data,
+      })
+      this.props.history.push(`${publicURL}/app/contacts`)
+    } catch (error) {
+      this.order = 'ASC'
+      this.setState({
+        isLoading: false,
+      })
     }
   }
 
   itemListRenderer = ItemList => (<ContactItemList itemList={ItemList} />)
 
   render() {
+    const toolbarItems = [
+      {
+        key: 'SortItem',
+        text: I18n.t('commons.sort'),
+        iconProps: { iconName: 'Sort' },
+        disabled: !this.state.selectedItems.length === 0,
+        checked: false,
+        style: { display: !this.props.selectionMode ? '' : 'none' },
+        onClick: this.handleSort,
+      },
+      {
+        key: 'refreshItem',
+        text: I18n.t('commons.refresh'),
+        iconProps: { iconName: 'Refresh' },
+        disabled: !this.state.selectedItems.length === 0,
+        checked: false,
+        style: { display: !this.props.selectionMode ? '' : 'none' },
+        onClick: this.handleRefresh,
+      },
+      {
+        key: 'EditItem',
+        text: I18n.t('commons.edit'),
+        iconProps: { iconName: 'Edit' },
+        disabled: this.state.selectedItems.length === 0,
+        checked: false,
+        style: { display: this.props.selectionMode ? '' : 'none' },
+      },
+      {
+        key: 'DeleteItem',
+        text: I18n.t('commons.delete'),
+        iconProps: { iconName: 'Delete' },
+        disabled: this.state.selectedItems.length === 0,
+        checked: false,
+        style: { display: this.props.selectionMode ? '' : 'none' },
+        onClick: this.dialogDelete,
+      },
+      {
+        key: 'SelectionItem',
+        text: I18n.t('commons.select'),
+        iconProps: { iconName: 'MultiSelectMirrored' },
+        disabled: false,
+        checked: false,
+        style: { display: '' },
+        onClick: this.handleToggleSelectionMode,
+      },
+    ]
+
+    let listComponent
+
     if (this.state.isLoading) {
-      return <Loader count={3} />
+      listComponent = <Loader count={3} />
+    } else if (this.state.itemList) {
+      if (this.state.itemList.length > 0) {
+        listComponent = (
+          <DetailsList
+            columns={this.columns}
+            items={this.state.itemList}
+            isHeaderVisible={false}
+            selectionMode={this.props.selectionMode ? SelectionMode.multiple : SelectionMode.none}
+            setKey="set"
+            selection={this.selection}
+            compact
+          />
+        )
+      } else {
+        listComponent = <EmptyMessage message={I18n.t('users.not_found')} icon={this.props.icon} showIcon />
+      }
+    } else {
+      listComponent = <EmptyMessage message={I18n.t('users.not_found')} icon={this.props.icon} showIcon />
     }
 
-    if (this.state.itemList) {
-      if (this.state.itemList.length > 0) {
-        return (
-          <React.Fragment>
-            <ToolBar />
-            <DetailsList
-              columns={this.columns}
-              items={this.state.items}
-              isHeaderVisible={false}
-              selectionMode={this.state.isModalSelection ? SelectionMode.multiple : SelectionMode.none}
-              setKey="set"
-              layoutMode={DetailsListLayoutMode.justified}
-              selection={this.selection}
-              compact
-            />
-          </React.Fragment>
-        )
-      }
-    }
-    return null
+    return (
+      <React.Fragment>
+        <CommandBar
+          items={toolbarItems}
+          onReduceData={() => {}}
+        />
+        {listComponent}
+      </React.Fragment>
+    )
   }
 }
 
-export default ContactList
+ContactList.defaultProps = {
+  action: '',
+}
+
+ContactList.propTypes = {
+  confirmation: PropTypes.object.isRequired,
+  selectedItems: PropTypes.array.isRequired,
+  changeSelectedItems: PropTypes.func.isRequired,
+  selectionMode: PropTypes.bool.isRequired,
+  history: PropTypes.object.isRequired,
+  changeSelectionMode: PropTypes.func.isRequired,
+  action: PropTypes.string,
+  changeAction: PropTypes.func.isRequired,
+  toast: PropTypes.object.isRequired,
+  icon: PropTypes.string.isRequired,
+}
